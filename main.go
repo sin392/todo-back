5f35cffa358cb41fc6a7e17e37ffc65c41126cb2
sin392 <sin392@gmail.com>
2021-10-11 12:26:05 +0900

    update uri

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	http.Handle("/edit", handler.NewTODOHandler(service.NewTODOService(db)))
+	http.Handle("/todo", handler.NewTODOHandler(service.NewTODOService(db)))
 
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal("ListenAndServe failed.", err)
